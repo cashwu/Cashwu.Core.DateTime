68bd26bd2f4b07b6ccc409a8cb86698818091c26
Cash Wu <cash@cashwu.com>
2019-07-30 09:47:26 +0800

    IsBefore method

---
+++ JavaLikeDateTime.Tests/IsBeforeTests.cs
@@ -0,0 +1,36 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace JavaLikeDateTime.Tests
+{
+    public class IsBeforeTests
+    {
+        [Fact]
+        public void Before_other_datetime()
+        {
+            var current = new DateTime(2019, 9, 1, 1, 00, 00, 000);
+            var other = new DateTime(2019, 9, 1, 1, 00, 00, 001);
+
+            current.IsBefore(other).Should().BeTrue();
+        }
+
+        [Fact]
+        public void After_other_datetime()
+        {
+            var current = new DateTime(2019, 9, 1, 1, 00, 00, 001);
+            var other = new DateTime(2019, 9, 1, 1, 00, 00, 000);
+
+            current.IsBefore(other).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equal_other_datetime()
+        {
+            var current = new DateTime(2019, 9, 1, 1, 00, 00, 000);
+            var other = new DateTime(2019, 9, 1, 1, 00, 00, 000);
+
+            current.IsBefore(other).Should().BeFalse();
+        }
+    }
+}
--- JavaLikeDateTime/JavaLikeDateTimeExtensions.cs
+++ JavaLikeDateTime/JavaLikeDateTimeExtensions.cs
@@ -14,5 +14,16 @@ namespace JavaLikeDateTime
         {
             return current > other;
         }
+        
+        /// <summary>
+        /// Checks if this current DateTime is before the specified (other) DateTime.
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool IsBefore(this DateTime current, DateTime other)
+        {
+            return current < other;
+        }
     }
 }
